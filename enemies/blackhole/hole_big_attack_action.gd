880a66bf7a4d829b2ea4a33595e7fb7873c5cf1e
Firea <93664604+Fireaempress@users.noreply.github.com>
2025-04-25 08:39:39 +0200

    Add files via upload

---
+++ enemies/blackhole/hole_big_attack_action.gd
@@ -0,0 +1,36 @@
+extends EnemyAction
+
+@export var attack := 10
+@export var hp_threshold := 10
+
+var already_used := false
+
+func is_performable() -> bool:
+	if not enemy or already_used:
+		return false
+	
+	var is_low := enemy.stats.health <= hp_threshold
+	already_used = is_low
+	
+	return is_low
+
+func perform_action() -> void:
+	if not enemy or not target:
+		return
+	var tween := create_tween().set_trans(Tween.TRANS_QUINT)
+	var start := enemy.global_position
+	var end := target.global_position + Vector2.RIGHT*400 + Vector2.UP*200
+	var attack_effect := DamageEffect.new()
+	var target_array : Array[Node] = [target]
+	attack_effect.amount = attack
+	
+	tween.tween_property(enemy, "global_position", end, 0.4)
+	tween.tween_callback(attack_effect.execute.bind(target_array))
+	tween.tween_interval(0.25)
+	tween.tween_property(enemy, "global_position", start, 0.4)
+	tween.finished.connect(
+		func():
+			Events.enemy_action_completed.emit(enemy)
+			
+	)
+
